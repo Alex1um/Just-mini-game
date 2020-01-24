381bdc4be5e17277d77ff76911743981f6fa61b3
assa <ye4h@protonmail.com>
2020-01-24 16:53:43 +0700

    initial

---
+++ main.py
@@ -0,0 +1,19 @@
+from core import *
+from engine import *
+import pygame
+from threading import Thread
+
+pygame.init()
+
+
+class Client:
+
+    def __init__(self):
+        # Todo realize Game ares
+        main_menu = GameArea()
+
+
+    def run(self):
+        while 1:
+            for event in pygame.event.get():
+                pass
